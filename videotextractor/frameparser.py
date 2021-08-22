05fa42201ed9918ab793d1182287114fdbf47177
Jyrki Kokkola <jyrki.j.kokkola@student.jyu.fi>
2021-08-22 04:01:58 +0300

    Refactored frameparser to more independent functions

--- videotextractor/frameparser.py
+++ videotextractor/frameparser.py
@@ -6,71 +6,81 @@ You can adjust the interval of the frame captures, the start and stop position
 of the capture process, wether to write the frames to the disk as image files,
 where to store them and in which format.
 
-TODO:
-    - Instead of writing the frames as image files, implement a version
-    of the function that keeps the frames in-memory for faster
-    processing (Like keep the images in an array)
 """
 import os
 import sys
 import cv2
+import numpy as np
 
 
 def extract_frames(
-    video_path: str, start: int = 0, stop: int = -1, interval: int = 1
-) -> tuple:
-    """Extract frames from a video source using VideoReader
+    video_source: cv2.VideoCapture, start: int, stop: int, interval: int
+) -> list[np.ndarray]:
+    """Extract frames from a video source and return them as ndarrays stored in
+    a list.
 
     Args:
-        video_path: Path to the video file.
-        start: Timestamp (in seconds) when to start extracting
-        stop: Timestamp (in seconds) when to stop extracting
-        interval: Seconds how far between each saved frame is
+        video_source (cv2.VideoCapture): Video capture
+        start (int): Timestamp (in seconds) when to start extracting
+        stop (int): Timestamp (in seconds) when to stop extracting
+        interval (int): Seconds how far between each saved frame is
 
     Returns:
-        Tuple, containing the path to the directory and count of
-        frames extracted.
+        List of frames stored as np.ndarrays.
     """
-    if not os.path.exists(video_path):
-        raise FileNotFoundError(f"File '{video_path}' not found.")
-    img_count = 0
-    video_dir, video_name = os.path.split(video_path)
-    frames_dir = f"{video_dir}/frames_{video_name[:-4]}"
-    os.makedirs(frames_dir, exist_ok=False)
-    cap = cv2.VideoCapture(video_path)
-    fps = cap.get(cv2.CAP_PROP_FPS)
+    frames = []
+    fps = video_source.get(cv2.CAP_PROP_FPS)
     if stop <= start:
-        stop = cap.get(cv2.CAP_PROP_FRAME_COUNT) // fps
+        stop = video_source.get(cv2.CAP_PROP_FRAME_COUNT) // fps
     for sec in range(start, stop, interval):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, (sec * 25))
-        ret, frame = cap.read()
+        video_source.set(cv2.CAP_PROP_POS_FRAMES, (sec * fps))
+        ret, frame = video_source.read()
         if not ret:
             raise cv2.error(f"Failed to retrieve frame at {sec * 25:.1f} sec.")
-        cv2.imwrite(f"{frames_dir}/{sec}.jpg", frame)
-        img_count += 1
-    return (frames_dir, img_count)
+        frames.append(frame)
+    return frames
 
 
-def save_images(dir_path: str, frames: list) -> int:
-    """Takes an array of images and saves them to a directory.
+def save_images(dir_path: str, frames: list[np.ndarray]) -> bool:
+    """Takes a list of images in np.ndarray format and saves them to a directory,
+    creating the directory if necessary.
 
     Args:
         dir_path (str): Path to the directory where to save the images.
         frames (list): List of cv2 frame captures.
 
     Returns:
-        int: Count of images saved.
+        bool: True if succesfull, False if not.
     """
     try:
-        os.makedirs(dir_path, exists_ok=False)
+        os.makedirs(dir_path, exist_ok=False)
+        print(f"Created directory '{dir_path}'...")
         count = 1
         for img in frames:
             cv2.imwrite(f"{dir_path}/{count}.jpg", img)
+            print(f"Writing image {count}.jpg...")
             count += 1
-        return count
+        print("Success!")
+        return True
     except FileExistsError as e:
         print(f"Error in creating the frame directory: {e}")
-        return 0
+        return False
+
+
+def extract_and_save(file_path: str, start: int = 0, stop: int = -1, interval: int = 1):
+    """Extract frames and then save them to a directory.
+
+    Args:
+        file_path (str): [description]
+        start (int, optional): [description]. Defaults to 0.
+        stop (int, optional): [description]. Defaults to -1.
+        interval (int, optional): [description]. Defaults to 1.
+    """
+    cap = cv2.VideoCapture(file_path)
+    video_dir, video_name = os.path.split(file_path)
+    frames_dir = f"{video_dir}/frames_{video_name[:-4]}"
+    frames = extract_frames(cap, start, stop, interval)
+    save_images(frames_dir, frames)
 
 
 def quicksave(video_path: str, stop: int) -> None:
@@ -83,15 +93,14 @@ def quicksave(video_path: str, stop: int) -> None:
         stop (int): Time (in seconds) where to stop extracting sceenshots.
     """
     try:
-        dir_path, img_count = extract_frames(video_path, 1, stop, 1)
-        print(f"Succesfully retrieved {img_count} frames to {dir_path}!")
+        extract_and_save(video_path, 1, stop, 1)
     except FileNotFoundError as e:
-        print(f'Error in the video file: {e}')
+        print(f"Error in the video file: {e}")
     except FileExistsError as e:
-        print(f'Error in creating the frame directory: {e}')
+        print(f"Error in creating the frame directory: {e}")
     except cv2.error as e:
         print(f"Frame extraction error: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     quicksave(sys.argv[1], int(sys.argv[2]))
